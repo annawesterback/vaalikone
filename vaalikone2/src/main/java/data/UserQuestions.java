df377b6e9f0ce2584f1962386af0a04037366035
johanna20158 <johanna20158@student.hamk.fi>
2022-04-29 13:44:54 +0300

    Kysymykset näkyy kandidaatille

---
+++ vaalikone2/src/main/java/data/UserQuestions.java
@@ -0,0 +1,84 @@
+package data;
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+import dao.Dao;
+
+/**
+ * Date: 23.4.2022
+ * Here are data values for questions in Election Machine program where admin can add, edit and remove questions.
+ * @author owner group 20M Anna Westerback, Ansa Holttinen and Johanna Sieranoja TRTKM20A3
+ * @version 1.0
+ * The persistent class for the kid database table.
+ */
+@Entity // tells that instances of this class are persistable
+@Table(name="kysymykset") // tämä oltava sama kuin tietokannan taulun nimi
+public class UserQuestions implements Serializable { // user questions olisi voinut olla kysymykset
+	private static final long serialVersionUID = 1L;
+	@Id // @Id All the Entities need an Id field. Here it is attribute id (int id;) and must be annotated with @Id.
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	/**
+	 * int value for question
+	 */
+	@Column(name="KYSYMYS_ID")
+	private int id;
+	/**
+	 * string value for question
+	 */
+	private String kysymys;
+
+	/**
+	 * @param id int value for question
+	 * @param kysymys string value for question
+	 */
+	public UserQuestions(String id, String kysymys) {
+		// TODO Auto-generated constructor stub
+		setId(id);
+		this.kysymys=kysymys;
+
+	}
+	/**
+	 * This method find questions
+	 */
+	public UserQuestions() {
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * This method get question using id
+	 * @return returns question
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * This method set question
+	 * @param id value for question
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * This method changes id from string to int
+	 * @param id value changed to int
+	 */
+	public void setId(String id) {
+		try {
+			this.id = Integer.parseInt(id);
+		}
+		catch(NumberFormatException | NullPointerException e) {
+			//Do nothing - the value of id won't be changed
+		}
+		
+	}
+	// KYSYMYS
+	/**
+	 * This method get questions from database
+	 * @return return question
+	 */
+	public String getKysymys() {
+		return kysymys;
+	}
+	public void setKysymys(String kysymys) {
+		this.kysymys = kysymys;
+	}
+}
--- vaalikone2/src/main/java/services/VaalikoneService.java
+++ vaalikone2/src/main/java/services/VaalikoneService.java
@@ -1,5 +1,53 @@
 package services;
 
-public class VaalikoneService {
+import java.io.IOException;
+import java.util.List;
+import dao.Dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+import data.UserQuestions;
+
+@Path("/vaalikoneservice")
+public class VaalikoneService {
+	@Context HttpServletRequest request;
+	@Context HttpServletResponse response;
+// lue kaikki rivit taulusta kysymykset
+	@GET
+	@Path("/all")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void readAllUserQuestions() {
+		//Create an EntityManagerFactory with the settings from persistence.xml file
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("vaalikone"); // EntityManagerFactory luo yhteys tietokantaan, tämä sama persistencessä, eli jpa käyttää tätä
+		//And then EntityManager, which can manage the entities.
+		EntityManager em=emf.createEntityManager();
+		
+		// Read all the rows from table prey. Here the Prey must start with capital, 
+		// because class's name starts. This returns a List of Prey objects.
+		List<UserQuestions> list=em.createQuery("select k from UserQuestions k").getResultList(); //Luokan nimi UserQuestions, siellä kerrottu että tämä luokka tarkoittaa tiekannassa @table kysymykset
+		// return list;
+		RequestDispatcher rd=request.getRequestDispatcher("/jsp/usershowquestions.jsp");
+		request.setAttribute("questionslist", list);
+		try {
+			rd.forward(request, response);
+		} catch (ServletException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 }
